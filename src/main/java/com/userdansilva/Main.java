453263beffccda12b711ab5c18fd3ee6d52baf6a
Daniel Silva <daniel.sousasilva97@outlook.com>
2023-02-09 07:25:36 -0300

    first end-point

---
+++ src/main/java/com/userdansilva/Main.java
@@ -0,0 +1,19 @@
+package com.userdansilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "This is fine";
+    }
+}
